27647e3fe3d2ea6f0bc4a283bf58068cccf4ce67
DIF <difakli049@gmail.com>
2022-12-10 14:48:37 +0100

    ajout de la table message

--- mydb.sql
+++ mydb.sql
@@ -1,11 +1,24 @@
-CREATE TABLE client
+CREATE TABLE if not exists client
 (
     id_client INT NOT NULL PRIMARY KEY,
     nom VARCHAR(50),
-    prenom VARCHAR(50), 
+    prenom VARCHAR(50),
     adr_mail VARCHAR(50),
     sexe ENUM('m','f'),
     date_naissance DATE,
+    photo varchar(250) NOT NULL,
     mdp VARCHAR(50),
-    client_admin boolean
+    statut int NOT NULL DEFAULT '0'
+);
+
+
+CREATE TABLE if not exists message
+(
+    id_message INT NOT NULL UNIQUE,
+    id_sender   INT NOT NULL REFERENCES client(id_client),
+    id_receiver   INT NOT NULL REFERENCES client(id_client),
+    message VARCHAR(2550),
+    date_heure DATETIME,
+
+    PRIMARY KEY(id_message, id_sender, id_receiver)
 );
