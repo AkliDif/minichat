70d45c57407d310784c2b7914cf59068ae0e2718
DIF <difakli049@gmail.com>
2022-12-11 21:01:02 +0100

    mise à jour de Chat

--- Chat.php
+++ Chat.php
@@ -1,51 +1,17 @@
 <?php
-require_once("config.php");
-if (empty($contenu))
-    {
+require_once("./inc/config.php");
 
-        if ($_POST)
-        {
-            $pseudo = $_SESSION['client']['pseudo'];
-            executeRequete("INSERT INTO Message (pseudo_receiver, pseudo_sender, Message) VALUES ('$_POST[pseudo]', '$pseudo','$_POST[message]')");
-        }
-    }
 $res = executeRequete("SELECT * FROM client");
 $contenu .= '<div class="Users">';
 $contenu .= "<ul>";
 while($user = $res->fetch_assoc())
 {
-    if ($user['pseudo'] != $_SESSION['client']['pseudo'])
-	    $contenu .= "<li><a href='discussion.php?pseudo="	. $user['pseudo'] . "'> "	. $user['pseudo'] . "</a></li>";
+    if ($user['id_client'] != $_SESSION['client']['id_client'])
+	    $contenu .= "<li><a href='discussion.php?id_receiver=" . $user['id_client'] . "'> "	. $user['nom'] .' ' . $user['prenom'] . "</a></li>";
 }
-$contenu .= "</ul>";
-$contenu .= "</div>";
-?>
-        <header class="mainHead">                    
-				<nav>
-					<?php
-					if(internauteEstConnecte())
-					{
-						echo '
-						<ul>
-						<a href="connexion.php?action=deconnexion">Déconnexion</a>
-                        </ul>';
-					}
-					else
-					{
-                        header("location:connexion.php");
-					}
 
-					?>
-				</nav>
-        </header>
-        <section>
-			
-<?php
-    
+    require_once('./inc/haut.inc.php');
     echo ("Bonjour ".$_SESSION['client']['nom'] ." ".$_SESSION['client']['prenom'] ."</br>");
-    $pseudo = $_SESSION['client']['pseudo'];
-    
     echo $contenu;
     
-
-
+    require_once('./inc/bas.inc.php');
