0f087f00f8a18d0764567599681c8a1f46eb41bd
DIF <difakli049@gmail.com>
2022-12-11 21:03:06 +0100

    ajoute les message dans la base de donées

---
+++ envoieMessage.php
@@ -0,0 +1,16 @@
+<?php
+require_once('./inc/config.php');
+
+if (empty($contenu))
+{
+    if ($_POST)
+    {
+
+        $id_receiver = $_POST['id_receiver'];
+        $id_sender = $_SESSION['client']['id_client'];
+        $msg = $_POST['message'];
+
+        executeRequete ("INSERT INTO message (id_sender, id_receiver, message, date_heure) VALUES ('$id_sender', '$id_receiver', '$msg', now())");
+        header("location:discussion.php?id_receiver= '.$id_receiver.' ");
+    }
+};
