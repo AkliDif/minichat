f9a4517029a000811057f1db1d2e32e32baf7d63
DIF <difakli049@gmail.com>
2022-12-11 21:03:29 +0100

    update

--- mydb.sql
+++ mydb.sql
@@ -1,11 +1,13 @@
+DROP TABLE client;
+DROP TABLE message;
+
+
 CREATE TABLE if not exists client
 (
-    id_client INT NOT NULL PRIMARY KEY,
+    id_client INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     nom VARCHAR(50),
     prenom VARCHAR(50),
     adr_mail VARCHAR(50),
-    sexe ENUM('m','f'),
-    date_naissance DATE,
     photo varchar(250) NOT NULL,
     mdp VARCHAR(50),
     statut int NOT NULL DEFAULT '0'
@@ -14,7 +16,7 @@ CREATE TABLE if not exists client
 
 CREATE TABLE if not exists message
 (
-    id_message INT NOT NULL UNIQUE,
+    id_message INT NOT NULL UNIQUE AUTO_INCREMENT,
     id_sender   INT NOT NULL REFERENCES client(id_client),
     id_receiver   INT NOT NULL REFERENCES client(id_client),
     message VARCHAR(2550),
